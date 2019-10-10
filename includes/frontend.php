c12b2617f70255042a7e7f882fb543b6aa4a8a67
Rafael Angeline <me@rafaelangeline.com>
2019-10-10 16:29:53 -0300

    Add templating system

--- includes/frontend.php
+++ includes/frontend.php
@@ -11,7 +11,7 @@ namespace SauCalAPI\Frontend;
  * Enqueue scripts.
  */
 function enqueue_custom_scripts() {
-	wp_register_script( 'saucal-main', SAUCAL_API_URL . '/assets/scripts/main.js', array( 'jquery' ), SAUCAL_API_VERSION, true );
+	wp_register_script( 'saucal-main', SAUCAL_API_URL . '/assets/scripts/main.js', array( 'jquery', 'underscore', 'backbone', 'wp-util' ), SAUCAL_API_VERSION, true );
 
 	wp_localize_script(
 		'saucal-main',
@@ -27,3 +27,11 @@ function enqueue_custom_scripts() {
 	wp_enqueue_script( 'saucal-main' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_custom_scripts' );
+
+/**
+ * Include template at Front.
+ */
+function include_template() {
+	require_once SAUCAL_API_PATH . 'templates/widget.php';
+}
+add_action( 'wp_footer', __NAMESPACE__ . '\include_template', 2000 );
---
+++ templates/widget.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Widget Output Template
+ *
+ * To overwrite this template, add the following file to your theme: /your-theme/saucal-api/widget.php.
+ * Reference: https://github.com/olado/doT
+ *
+ * @package SauCalAPI
+ */
+
+?>
+
+<script id="saucal-api-widget" type="text/html">
+	<p>{{=it.title}}</p>
+	<p><small>{{=it.author}}</small></p>
+</script>
